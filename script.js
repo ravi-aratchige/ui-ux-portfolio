9fd549594613b2c04eafc12e65aedfb93f1f2a97
ravi-aratchige <rmaratchige@gmail.com>
2022-12-11 12:00:40 +0530

    Added testing backend functionality

---
+++ script.js
@@ -0,0 +1,13 @@
+function SubForm (){
+    $.ajax({
+        url:"https://api.apispreadsheets.com/data/qkZ7Z3W6U2i3whui/",
+        type:"post",
+        data:$("#myForm").serializeArray(),
+        success: function(){
+            alert("Form Data Submitted :)")
+        },
+        error: function(){
+            alert("There was an error :(")
+        }
+    });
+}
